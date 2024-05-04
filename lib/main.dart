d992290b19d9882a266ec126421bba8dfeb6de09
saraUsama9910 <sarausama80@gmail.com>
2024-05-04 08:35:17 +0300

    install new package carousel slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,102 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: chatbot(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class chatbot extends StatefulWidget {
+  const chatbot({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<chatbot> createState() => _chatbotState();
+}
+
+class _chatbotState extends State<chatbot> {
+  final List<String> imageList = [
+    'https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?cs=srgb&dl=background-blur-clean-531880.jpg&fm=jpg',
+    'https://th.bing.com/th/id/OIP.VtyV1I7hrXKYkHVQUPkcfgHaEK?rs=1&pid=ImgDetMain',
+    'https://th.bing.com/th/id/R.9e0510fce40905a7ce9b1dde63e69032?rik=WGxTdyWR5zsZ7g&riu=http%3a%2f%2ffc00.deviantart.net%2ffs70%2fi%2f2011%2f147%2f9%2fa%2fnight_sky_by_vectorday-d3hc4p8.jpg&ehk=0FOSGYP9O%2fPHW0csbrEm6XjglBrqsLdNs6bCHUWct4k%3d&risl=&pid=ImgRaw&r=0',
+    'https://th.bing.com/th/id/R.68acc506789f5d343c1280f3bc92475d?rik=dZDbqULavRoElQ&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f06%2fMacbook-Pro-13-Inch-HD-Wallpaper.jpg&ehk=RvWoWqG58SLfhLVYgqbu9PMxYPBBS0WkRqeEmZYMPjc%3d&risl=&pid=ImgRaw&r=0',
+    'https://th.bing.com/th/id/OIP.dowJnnydaFIDDTdR_C8kiwHaEo?rs=1&pid=ImgDetMain',
+    'https://th.bing.com/th/id/OIP.oM5INDF08ka_cU7csjgSLgHaHa?rs=1&pid=ImgDetMain',
+  ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Chat-Bot'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView(
+        padding: const EdgeInsets.only(top: 30),
+        children: [
+          Center(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                enableInfiniteScroll: true,
+                autoPlay: true,
+              ),
+              items: imageList
+                  .map(
+                    (e) => ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          Image.network(
+                            e,
+                            width: 1050,
+                            height: 350,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const Divider(
+            height: 50,
+          ),
+          Card(
+            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            child: Image.network(
+              'https://th.bing.com/th?id=OIF.gqRXaSg%2bJHd0uJ17j0%2fVkQ&rs=1&pid=ImgDetMain',
+              fit: BoxFit.fill,
+              height: 150,
+              width: 150,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            width: double.infinity,
+            height: 120,
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      'AI 4 Years',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 135,child: Divider(),),
+                    Text('Dr.Ahmed Taha'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
